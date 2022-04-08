139f68ad49422e57c415de87e48cada0eee26dd1
Aniruddha <aniruddha1712@gmail.com>
2022-04-08 15:45:34 +0530

    [Aniruddha]Added: Modified program to fina Max out of three Strings

--- FindMaxWithGenerics/Maximum.cs
+++ FindMaxWithGenerics/Maximum.cs
@@ -46,5 +46,24 @@ namespace FindMaxWithGenerics
                 return 0;
             }
         }
+        public string MaxOutOfThree(string num1, string num2, string num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
--- FindMaxWithGenerics/Program.cs
+++ FindMaxWithGenerics/Program.cs
@@ -10,10 +10,11 @@ namespace FindMaxWithGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Find Maximum using GENERICS program");
+            Console.WriteLine("Welcome to the Find Maximum using GENERICS program\n");
 
             Maximum maximum = new Maximum();
-            Console.WriteLine("Choose the option below.....\n1. Find Max Of Three Int Number\n2. Find Max of Three Float values\n");
+            Console.WriteLine("Choose the option below.....\n1. Find Max Of Three Int Number\n2. Find Max of Three Float values" +
+                "\n3. Find Max Of Three Strings\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -47,6 +48,22 @@ namespace FindMaxWithGenerics
                     else
                         Console.WriteLine("Maximum out of given numbers {0} {1} & {2} is {3}", floatNum1, floatNum2, floatNum3, floatMax);
                     break;
+                case 3:
+                    Console.WriteLine("Enter first string");
+                    string str1 = Console.ReadLine();
+                    Console.WriteLine("Enter second string");
+                    string str2 = Console.ReadLine();
+                    Console.WriteLine("Enter third string");
+                    string str3 = Console.ReadLine();
+                    string strMax = maximum.MaxOutOfThree(str1, str2, str3);
+                    if (strMax == null)
+                    {
+                        Console.WriteLine("Two or Three strings are same");
+                    }
+                    else
+                        Console.WriteLine("Maximum out of given strings {0} {1} & {2} is {3}", str1, str2, str3, strMax);
+                    break;
+
                 default:
                     Console.WriteLine("Invalid Input");
                     break;
