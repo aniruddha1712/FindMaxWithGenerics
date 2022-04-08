2eb790bace8cc1e0133a050a19944d61c55b6618
Aniruddha <aniruddha1712@gmail.com>
2022-04-08 15:11:04 +0530

    [Aniruddha]Added: Program to find Max out of three integers

--- FindMaxWithGenerics/Program.cs
+++ FindMaxWithGenerics/Program.cs
@@ -11,6 +11,31 @@ namespace FindMaxWithGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Find Maximum using GENERICS program");
+
+            Maximum maximum = new Maximum();
+            Console.WriteLine("Choose the option below.....\n1. Find Max Of Three Int Number\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first integer");
+                    int intNum1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second integer");
+                    int intNum2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third integer");
+                    int intNum3 = Convert.ToInt32(Console.ReadLine());
+                    int intMax = maximum.MaxOutOfThree(intNum1, intNum2, intNum3);
+                    if(intMax==0)
+                    {
+                        Console.WriteLine("Two or Three numbers have equal value");
+                    }
+                    else
+                        Console.WriteLine("Maximum out of given numbers {0} {1} & {2} is {3}",intNum1,intNum2,intNum3,intMax);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
             Console.ReadLine();
         }
     }
